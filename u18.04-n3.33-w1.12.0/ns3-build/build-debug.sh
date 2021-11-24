9a20c6904d479f90a528a8c2a6f7e6f7e9b4e238
Emanuele Giona <giona.emanuele@gmail.com>
2021-11-24 12:36:36 +0100

    updated copyright text

--- u18.04-n3.33-w1.12.0/ns3-build/build-debug.sh
+++ u18.04-n3.33-w1.12.0/ns3-build/build-debug.sh
@@ -2,7 +2,8 @@
 
 # MIT License
 
-# Copyright (c) 2021 Emanuele Giona <giona.emanuele@gmail.com> (SENSES Lab)
+# Copyright (c) 2021 Emanuele Giona <giona.emanuele@gmail.com> (SENSES Lab, 
+# Sapienza University of Rome)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
